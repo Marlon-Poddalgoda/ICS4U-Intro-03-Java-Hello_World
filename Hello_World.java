ed56237a4633a0487a7d3caa989f923022b92281
Ubuntu <ubuntu@ip-172-31-67-167.ec2.internal>
2021-04-20 16:03:48 +0000

    Initial Commit

---
+++ Hello_World.java
@@ -0,0 +1,9 @@
+/**
+ * Created By: Marlon Poddalgoda
+ * Class Hello_World
+ */
+public class Hello_World {
+	public static void main(String[] args) {
+    	System.out.println("Hello, World!");
+	}
+}
